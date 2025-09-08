39f1f26d4006c58727ea5db616515bb9a2345eeb
Muhammad Huzaifa <huzaifa818481@gmail.com>
2025-09-08 22:32:31 +0500

    create add, edit, delete button in todo list

--- Todo app/src/App.jsx
+++ Todo app/src/App.jsx
@@ -1,14 +1,33 @@
 import "./App.css";
 import Navbar from "./component/Navbar";
 function App() {
- 
+  const handleDelete=()=>{
+   console.log("delete"); 
+  }
+  const handleEdit=()=>{
+    console.log("edit");
+  }
+ const handleAdd=()=>{
+  console.log("add");
+ }
   return(
     <>
     <Navbar/>
-    <div className="container mx-auto">
-    <div className="bg-blue-400 text-white text-center p-4 rounded-md mt-4">
-      <h1>Your TODO</h1>
+    <div className="container mx-auto bg-gray-500 text-white p-4 rounded-md mt-4 min-h-[80vh]">
+    <div className="addTodo">
+      <h2>Add a todo</h2>
+      <input className="bg-gray-400 w-70" type="text" placeholder="Add todo" />
+      <button onClick={handleAdd} className="cursor-pointer bg-violet-800 hover:bg-violet-950 p-3 py-1 text-amber-50 rounded-lg mx-2">Add</button>
     </div>
+      <h1 className="text-xl font-bold">Your TODO</h1>
+      <div className="todos">
+        <div className="todo flex">
+        <div className="text">Lorem, ipsum dolor sit amet consectetur adipisicing.</div>
+        <button onClick={handleDelete} className="cursor-pointer bg-violet-800 hover:bg-violet-950 p-3 py-1 text-amber-50 rounded-lg mx-1">delete</button>
+        <button onClick={handleEdit} className="cursor-pointer bg-violet-800 hover:bg-violet-950 p-3 py-1 text-amber-50 rounded-lg mx-1">edit</button>
+        </div>
+        </div>    
+
     </div>
     </>
   )
--- Todo app/src/component/Navbar.jsx
+++ Todo app/src/component/Navbar.jsx
@@ -3,12 +3,12 @@ import React from "react";
 function Navbar() {
   return (
     <nav className="bg-gray-800 text-white p-4 flex justify-between">
-      <div className="logo cursor-pointer hover:text-blue-400">
+      <div className="logo cursor-pointer">
         <span className="font-black">Total Task</span>
       </div>
       <div className="flex gap-10 justify-end">
-        <span className="cursor-pointer hover:text-blue-400 hover-underline">Home</span>
-        <span className="cursor-pointer hover:text-blue-400 hover-underline">About us</span>
+        <span className="cursor-pointer hover-underline">Home</span>
+        <span className="cursor-pointer hover-underline">About us</span>
       </div>
     </nav>
   );
