59ac285461c204a0f20c29a2921f9f64b145d36d
Muhammad Huzaifa <huzaifa818481@gmail.com>
2025-09-08 20:08:27 +0500

    designing in navbar

--- Todo app/src/component/Navbar.jsx
+++ Todo app/src/component/Navbar.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
   return (
-    <nav className=' bg-gray-800 text-white p-4 flex justify-between'>
-        <div className="logo cursor-pointer">
-            <span className='font-black '>Total Task</span>
-        </div>
-      <ul className='flex gap-10 justify-end '>
-        <li className='cursor-pointer'>Home</li>
-        <li className='cursor-pointer'>About us</li>
-        <li></li>
-      </ul>
+    <nav className="bg-gray-800 text-white p-4 flex justify-between">
+      <div className="logo cursor-pointer hover:text-blue-400">
+        <span className="font-black">Total Task</span>
+      </div>
+      <div className="flex gap-10 justify-end">
+        <span className="cursor-pointer hover:text-blue-400 hover-underline">Home</span>
+        <span className="cursor-pointer hover:text-blue-400 hover-underline">About us</span>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
